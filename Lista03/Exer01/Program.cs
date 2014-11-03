0b018cf2e4b57d78d575d0aa889607273a711a3c
Jefferson <jeffersonxplode@gmail.com>
2014-11-03 20:31:36 -0300

    Primeiro exercicio feito sem maiores problemas

---
+++ Lista03/Exer01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circulo bola = new Circulo();
+
+            Console.WriteLine("Digite o Raio");
+
+            bola.SetRaio(System.Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine(bola.calculo());
+
+            Console.ReadKey();
+        
+        }
+    }
+}
---
+++ Lista03/Exer01/circulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc1
+{
+    class Circulo
+    {
+
+        private double area;
+        private double raio;
+        private const double pi = Math.PI;
+
+        
+        public Circulo(){
+        
+        }    
+    
+        public void SetRaio (double raio) {
+
+            this.raio = raio;
+        
+        }
+
+
+        public double calculo() {
+
+
+            return area = pi * (raio * 2);
+            
+        }
+
+        
+
+
+
+    }
+}
