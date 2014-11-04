c1686fa45d037c20c20652d5c83cfb998ad1684e
Jefferson <jeffersonxplode@gmail.com>
2014-11-03 23:14:24 -0300

    Exercicio 4 feito, facil mas um pouco trabalhoso

---
+++ Lista03/exer04/PAPG.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer04
+{
+    class PAPG
+    {
+
+        private int numero, razao;
+        
+        private string prog;
+
+  
+       
+        
+        public PAPG(int numero, int razao, string prog)
+        {
+
+            this.numero = numero;
+            this.prog = prog;
+            this.razao = razao;
+        }
+
+
+        public PAPG()
+        {
+        }
+
+        public void SetNumero(int numero)
+        {
+
+            this.numero = numero;
+
+        }
+
+        public void SetRazao(int razao)
+        {
+
+            this.razao = razao;
+
+        }
+
+        public void SetProg(string prog)
+        {
+
+            this.prog = prog;
+
+        }
+
+
+
+        public void elementos(int n)
+        {
+
+            int somatoria = numero;
+
+
+            if (prog == "PA"){
+
+                for (int i = 0; i < n; i++)
+                {
+
+
+                    somatoria = somatoria + razao;
+
+                    Console.WriteLine(somatoria);
+                }
+            }
+
+             if (prog == "PG"){
+
+                for (int i = 0; i < n; i++) { 
+                    
+                    
+                    somatoria = somatoria * razao;
+                    
+                    Console.WriteLine(somatoria);
+                }
+           }
+      }
+
+        public void nesimo(int n)
+        {
+
+            int somatoria = numero;
+
+            if (prog == "PA")
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    somatoria = somatoria + razao;
+
+                }
+
+                Console.WriteLine(somatoria);
+            
+            }
+
+            if (prog == "PG")
+            {
+             
+                for (int i = 0; i < n; i++)
+                {
+
+                    somatoria = somatoria * razao;  
+               
+                }
+
+                Console.WriteLine(somatoria);
+            }
+        }
+
+
+        public void somas(int n)
+        {
+
+            int somatoria = 1;
+            int soma = 0;
+
+            if (prog == "PA")
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    somatoria = somatoria + razao;
+                    soma = somatoria + soma;
+                }
+
+                Console.WriteLine(soma);
+
+            }
+
+            if (prog == "PG")
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    somatoria = somatoria * razao;
+                    soma = somatoria + soma;
+                }
+
+                Console.WriteLine(soma);
+            }
+
+
+
+
+
+        }
+
+    
+    }
+}
---
+++ Lista03/exer04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PAPG papg = new PAPG();
+
+            Console.WriteLine("Digite o primeiro numero");
+
+            papg.SetNumero(System.Convert.ToInt16(Console.ReadLine()));
+
+            Console.WriteLine("Digite a razao");
+
+            papg.SetRazao(System.Convert.ToInt16(Console.ReadLine()));
+
+            Console.WriteLine("Digite o tipo de progressão (PA OU PG)");
+
+            papg.SetProg((Console.ReadLine()));
+
+            Console.WriteLine("Digite os quantos n elementos da progressão serão imprimidos");
+
+            papg.elementos(System.Convert.ToInt16(Console.ReadLine()));
+
+            Console.WriteLine("Digite o N elemento da progressão");
+
+            papg.nesimo(System.Convert.ToInt16(Console.ReadLine()));
+
+            Console.WriteLine("Digite o N da soma dos N elementos");
+
+            papg.somas(System.Convert.ToInt16(Console.ReadLine()));
+
+            Console.ReadKey();
+
+        }
+    }
+}
