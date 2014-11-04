b1045e8a1d710ad1af4d486841681b439f3ef66e
Jefferson <jeffersonxplode@gmail.com>
2014-11-03 23:42:47 -0300

    Odeio Matematica
    Odeio matematica por isso nem vou fazer isso D:

---
+++ Lista03/Exer02/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer02
+{
+    class Racional
+    {
+
+        private int numerador;
+        private int denominador;
+
+        Racional(int numerador, int denominador)
+        {
+
+            this.numerador = numerador;
+            this.denominador = denominador;
+
+        }
+
+    }
+}
